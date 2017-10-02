f44c16a5545d48b46dc1dc2ecae98735e7e77036
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-01 22:39:04 -0500

    Css y base de JSX

---
+++ app.jsx
@@ -0,0 +1,11 @@
+class ABCquiz extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return ();
+    }
+}
+
+ReactDOM.render(<ABCquiz />, document.getElementById("container"));
