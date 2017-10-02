6ab90f0b2bc994ca65fe08e3cf2c585437635cc0
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-01 23:11:25 -0500

    copiando html

--- app.jsx
+++ app.jsx
@@ -4,7 +4,28 @@ class ABCquiz extends React.Component {
     }
 
     render() {
-        return ();
+        return (
+            <section className="container">
+                <div id="abc-game" className="text-center abc-game">
+                    <div><img id="prev" src="img/left.svg" alt=""/></div>
+                    <div><img id="next" src="img/right.svg" alt=""/></div>
+                    <img id='vehicle' src="img/1.svg" alt=""/>
+                    <p id="textProgress" className='text-left'>0 of 5 answered</p>
+                    
+                    <div className="bg-white text-center" id="game">
+                        <h4 id="question"></h4>
+                        <div id="choices" className="row text-center choices"></div>
+                        <div className="social">
+                            <div className='circle'><img src="img/fb.png" alt=""/></div>
+                            <div className='circle'><img src="img/tw.png" alt=""/></div>
+                            <div className='circle'><img src="img/g+.png" alt=""/></div>
+                        </div>
+                    </div>
+                    <div className="bg-white text-center" id="result"></div>
+                    <div className="bg-white text-center" id="again"></div>
+                </div>
+            </section>
+        );
     }
 }
 
